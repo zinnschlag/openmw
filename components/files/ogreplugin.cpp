9e9e79991763de9f2d4b2a7dee7ab34e58a768a7
Michal Sciubidlo <michal.sciubidlo@gmail.com>
2013-01-09 16:58:37 +0100

    Don't mix release and debug ogre and openmw on windows.

--- components/files/ogreplugin.cpp
+++ components/files/ogreplugin.cpp
@@ -31,7 +31,13 @@ bool loadOgrePlugin(const std::string &pluginDir, std::string pluginName, Ogre::
         ogreRoot.loadPlugin(pluginPath);
         return true;
     }
-#endif
+    else {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        return false;
+#endif //OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    }
+#endif //defined(DEBUG)
+
     pluginPath = pluginDir + "/" + pluginName + pluginExt;
     if (boost::filesystem::exists(pluginPath)) {
     	ogreRoot.loadPlugin(pluginPath);
