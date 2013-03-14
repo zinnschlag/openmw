dce028fa92f29c0b8054c510205a2ad2aada5e64
Glorf <michal1.bien@gmail.com>
2013-03-14 12:59:52 +0100

    Minor fix, fallback system works

--- apps/openmw/mwworld/fallback.cpp
+++ apps/openmw/mwworld/fallback.cpp
@@ -32,13 +32,13 @@ namespace MWWorld
     Ogre::ColourValue Fallback::getFallbackColour(const std::string& fall)
     {
         std::string sum;
-	    std::string ret[3];
-	    sum=getFallbackString(fall);
-	    unsigned int j=0;
-	    for(unsigned int i=0;i<sum.length();i++){
-		    if(sum[i]==',') j++;
-		    else ret[j]+=sum[i];
-	    }
-	    return Ogre::ColourValue(boost::lexical_cast<int>(ret[0])/255.f,boost::lexical_cast<int>(ret[1])/255.f,boost::lexical_cast<int>(ret[2])/255.f);
+        std::string ret[3];
+        sum=getFallbackString(fall);
+        unsigned int j=0;
+        for(unsigned int i=0;i<sum.length();i++){
+            if(sum[i]==',') j++;
+            else ret[j]+=sum[i];
+        }
+        return Ogre::ColourValue(boost::lexical_cast<int>(ret[0])/255.f,boost::lexical_cast<int>(ret[1])/255.f,boost::lexical_cast<int>(ret[2])/255.f);
     }
 }
