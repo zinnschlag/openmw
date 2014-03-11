69d7661e54f686d74d28a393f5176d64bfb8ebe5
cc9cii <cc9c@iinet.net.au>
2014-03-11 18:56:44 +1100

    Bug #1185 fix

--- apps/openmw/mwstate/statemanagerimp.cpp
+++ apps/openmw/mwstate/statemanagerimp.cpp
@@ -220,7 +220,7 @@ void MWState::StateManager::loadGame (const Character *character, const Slot *sl
 {
     try
     {
-        cleanup();
+        cleanup(true); // force cleanup before loading the records from the save
 
         mTimePlayed = slot->mProfile.mTimePlayed;
 
